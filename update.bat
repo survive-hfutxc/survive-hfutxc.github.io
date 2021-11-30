63f2bb10ccc8ab15cd0c222b657339fd085d1654
survive-hfutxc <survivehfutxc@outlook.com>
2021-11-30 18:39:55 +0800

    测试hsudfia

--- update.bat
+++ update.bat
@@ -8,10 +8,12 @@ git config --local user.email "survivehfutxc@outlook.com"
 
 set input="May Diana with You"
 echo Default:May Diana with You
-set /p input=git commit -m 
+set /p input=git commit -m
 
 git add *
 git commit -m %input%
 git push
 
 pause
+
+# 测试下能不能上传
